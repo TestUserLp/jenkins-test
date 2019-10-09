78d46fe02d363b0c4171cfebd3e40f774c3a575e
Orest Lavriv <olavriv@gmail.com>
2019-10-09 20:10:06 +0300

    Added packages

--- scripted-test.groovy
+++ scripted-test.groovy
@@ -2,7 +2,11 @@ node('ubuntu') {
   stage('First stage') {
     sh 'uname -a'
   }
-  stage('Second stage') {
+  stage('Dependency check') {
+    sh 'apt-get update'
+    sh 'apt-cache show httpd'
+  }
+  stage('Last stage') {
     echo 'Hello'
   }
 }
