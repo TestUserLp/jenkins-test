41a6a04dccc53b0307890c661eeec6c02e24cd06
Orest Lavriv <olavriv@gmail.com>
2019-10-09 20:31:27 +0300

    Added holding

--- scripted-test.groovy
+++ scripted-test.groovy
@@ -4,7 +4,10 @@ node('ubuntu') {
   }
   stage('Dependency check') {
     sh 'sudo apt-get update'
+    sh 'sudo apt-cache policy apache2'
     sh 'sudo apt-cache show apache2'
+    sh 'sudo apt-mark hold apache2'
+    sh 'sudo apt-mark showhold'
   }
   stage('Last stage') {
     echo 'Hello'
